08680929dc5ed5838544913d4f651a0634bebc9e
lyushisyan <liushixian2019@gmail.com>
2026-04-04 15:53:32 +0300

    add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ language = "en"
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-html_logo = "_static/logo.jpg"
+html_logo = "_static/logo.png"
 
 html_theme_options = {
     "navigation_depth": 4,
